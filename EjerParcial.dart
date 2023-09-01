27a804a35dd4a6b2f1ac134701926c2ff8ad4478
Edgardo Cervantes Jattin <cerjattin@gmail.com>
2023-09-01 11:51:12 -0500

    validacion fecha

--- EjerParcial.dart
+++ EjerParcial.dart
@@ -4,25 +4,50 @@
 //c.Mostrar el total que se debe pagar
 import 'dart:io';
 
-class Producto{
-  String _nombre="";
-  int? Precio;
+class Producto {
+  String _nombre = "";
+  String? Precio;
   var Fecha;
-  set nombre(String nombre){
-    if(nombre==""){
-      throw("El nombre esta vacio");
+  set nombre(String nombre) {
+    if (nombre == "") {
+      throw ("El nombre esta vacio");
     }
-    _nombre=nombre;
+    _nombre = nombre;
   }
+
   String get nombre => _nombre;
 }
-void main(){
-final Producto produc=Producto();
-  stdout.write("Nombre del Producto: ");
-  String? nombre = stdin.readLineSync();
-  produc.nombre = nombre!;
-  stdout.write("Precio del Articulo: ");
-  int? precio = stdin.readLineSync() as int?;
-  produc.Precio =precio!;
-  print('Producto Ingresado ${produc.nombre}, El Precio del Articulo es: ${produc.Precio}');
+
+void main() {
+  final Producto produc = Producto();
+
+  List<Producto>;
+  String rep = 'S';
+  int i = 0;
+  int? tot = 0;
+  do {
+    stdout.write("Nombre del Producto: ");
+    String? nombre = stdin.readLineSync();
+    produc.nombre = nombre!;
+    stdout.write("Precio del Articulo: ");
+    String? precio = stdin.readLineSync();
+    produc.Precio = precio;
+    stdout.write("Fecha de Vencimiento de producto: ");
+    var fecha = stdin.readLineSync();
+    var fvenc = fecha;
+    fecha = fecha?.replaceAll('/', '-');
+    produc.Fecha = fecha;
+    final DateTime date = DateTime.parse(fecha!);
+    final DateTime now = DateTime.now();
+        if(date.compareTo(now)){
+          tot=(tot!+(int.parse(precio!)*1.15)) as int?;
+        }
+        tot=(tot!+(int.parse(precio!)));
+    print(
+        'Producto Ingresado ${produc.nombre}, El Precio del Articulo es: ${produc.Precio} ; El producto vence el : ${produc.Fecha}');
+    stdout.write("Desea continuar  ingresando Productos (S/N): ");
+    String? rep = stdin.readLineSync();
+    i++;
+  } while (rep == 'S');
+  print('Total de Productos Seleccionados: $tot');
 }
